4b9a415569183d4f1a5dcf06eb7dc9947005d9f7
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-09-13 21:28:30 +0300

    Добавьте файлы проекта.

---
+++ Homework1_3_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase="Каждый охотник желает знать сколько тут слов";
+            string[] phraseArray = phrase.Split(' ');
+
+            foreach (string word in phraseArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
